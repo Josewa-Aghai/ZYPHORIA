402b86d1fe350709a231127c06a4699e7322f0e3
jeevesh56 <YOUR_GITHUB_EMAIL>
2026-04-02 23:57:14 +0530

    feat: Implement Events Section UI and remove Team tab

---
+++ dist/server/assets/_tanstack-start-manifest_v-BNHp4mLN.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "C:/Users/josew/ZYPHORIA/src/routes/__root.tsx", "children": ["/", "/products/$productId"], "assets": [{ "tag": "link", "attrs": { "rel": "stylesheet", "href": "/assets/main-YKF4nWiE.css", "type": "text/css" } }], "preloads": ["/assets/main-BSJUbpFM.js"] }, "/": { "filePath": "C:/Users/josew/ZYPHORIA/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-BgMTexLY.js"] }, "/products/$productId": { "filePath": "C:/Users/josew/ZYPHORIA/src/routes/products/$productId.tsx", "assets": [], "preloads": ["/assets/_productId-DQLXagXp.js"] } }, "clientEntry": "/assets/main-BSJUbpFM.js" });
+export {
+  tsrStartManifest
+};
